25d972bcd2148a06697495490ef4818b8b8df324
mehrdad rdn <radnia.mehrdad@gmail.com>
2023-11-09 21:43:41 +0330

    starter init

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+function App() {
+  return (
+    <>
+      <h1 className="text-uppercase">starter content</h1>
+      {/* <Button variant="primary">primary</Button> */}
+      <Button variant="primary">Primary</Button>
+    </>
+  );
+}
+
+export default App;
